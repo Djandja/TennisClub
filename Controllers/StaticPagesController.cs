380711d4bcdb3ee77f7779dadfe45744010d16be
Djandja <andjelabor@gmail.com>
2020-08-20 12:01:26 +0200

    Add static pages and database, identity

---
+++ Controllers/StaticPagesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TennisClub.Controllers
+{
+    public class StaticPagesController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult TennisSchool()
+        {
+            return View();
+        }
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+
+        public IActionResult Turnirs()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TennisClub.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Username:")]
+        public override string UserName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Full name:")]
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Role:")]
+        public string RoleName { get; set; }
+    }
+}
